01e0f20087cedcf0a01fe96b31205ca4ef2d668c
daniloeder <daniloeder1997@gmail.com>
2023-10-25 10:39:47 -0300

    Refactor the application for better code structure and readability

--- src/components/QuizPage.vue
+++ src/components/QuizPage.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+import generateQuestion from "@/utils/questionGenerator";
+
 export default {
   data() {
     return {
@@ -19,18 +21,12 @@ export default {
       questionNumber: 1,
       timeLeft: 10,
       timerInterval: null,
-      timeoutId: null,
-      difficulty: this.$route.query.difficulty || 'easy', // set difficulty based on route query or default to 'easy'
-      difficultyMapping: {
-        easy: 10,
-        medium: 20,
-        hard: 50
-      }
+      timeoutId: null
     };
   },
   created() {
     this.startTimer();
-    this.generateQuestion();
+    this.generateNewQuestion();
   },
   methods: {
     startTimer() {
@@ -49,58 +45,18 @@ export default {
         }
       }, 1000);
     },
-    generateQuestion() {
-      // Check if we've already completed 10 questions
+    generateNewQuestion() {
       if (this.questionNumber > 10) {
         this.feedback = "Quiz completed!";
         if (this.timerInterval) {
           clearInterval(this.timerInterval);
         }
-        return; // Don't generate another question
+        return;
       }
 
-      const maxNum = this.difficultyMapping[this.difficulty];
-      let num1 = Math.floor(Math.random() * maxNum) + 1;
-      let num2 = Math.floor(Math.random() * maxNum) + 1;
-
-      // Select operation based on question number
-      const operations = ["+", "-", "*", "/"];
-      const operation = operations[(this.questionNumber - 1) % operations.length];
-
-      switch (operation) {
-        case "+":
-          this.question = {
-            text: `What is ${num1} + ${num2}?`,
-            answer: num1 + num2
-          };
-          break;
-        case "-":
-          if (num2 > num1) {
-            [num1, num2] = [num2, num1]; // swap to ensure positive result
-          }
-          this.question = {
-            text: `What is ${num1} - ${num2}?`,
-            answer: num1 - num2
-          };
-          break;
-        case "*":
-          this.question = {
-            text: `What is ${num1} * ${num2}?`,
-            answer: num1 * num2
-          };
-          break;
-        case "/":
-          while (num1 % num2 !== 0) {
-            num2 = Math.floor(Math.random() * maxNum) + 1;
-          }
-          this.question = {
-            text: `What is ${num1} / ${num2}?`,
-            answer: num1 / num2
-          };
-          break;
-      }
-
-      this.answer = ""; // Clear the previous answer
+      const difficulty = this.$route.query.difficulty || 'easy';
+      this.question = generateQuestion(difficulty);
+      this.answer = "";
       this.startTimer();
     },
     submitAnswer() {
@@ -118,7 +74,7 @@ export default {
       this.timeoutId = setTimeout(() => {
         this.feedback = "";
         this.questionNumber++;
-        this.generateQuestion();
+        this.generateNewQuestion();
       }, 2000);
     }
   }
---
+++ src/utils/questionGenerator.js
@@ -0,0 +1,40 @@
+export default function generateQuestion(difficulty) {
+  const operations = ['+', '-', '*', '/'];
+  const maxValues = {
+    easy: 10,
+    medium: 50,
+    hard: 100
+  };
+  
+  let num1 = Math.floor(Math.random() * maxValues[difficulty]);
+  let num2 = Math.floor(Math.random() * maxValues[difficulty]);
+  
+  const operation = operations[Math.floor(Math.random() * operations.length)];
+
+  let text, answer;
+
+  switch (operation) {
+    case '+':
+      text = `What is ${num1} + ${num2}?`;
+      answer = num1 + num2;
+      break;
+    case '-':
+      if (num2 > num1) {
+        [num1, num2] = [num2, num1]; // Swap the numbers to avoid negative answers
+      }
+      text = `What is ${num1} - ${num2}?`;
+      answer = num1 - num2;
+      break;
+    case '*':
+      text = `What is ${num1} x ${num2}?`;
+      answer = num1 * num2;
+      break;
+    case '/':
+      num2 = num2 || 1;  // Avoid division by zero
+      text = `What is ${num1} / ${num2}?`;
+      answer = Math.round(num1 / num2);
+      break;
+  }
+
+  return { text, answer };
+}
