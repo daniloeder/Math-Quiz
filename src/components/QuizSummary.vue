0d54b637f1217a4b8da768c140e3c64f687e1d21
daniloeder <daniloeder1997@gmail.com>
2023-10-25 12:02:02 -0300

    Display the number of incorrect answers in the summary page

--- src/components/QuizSummary.vue
+++ src/components/QuizSummary.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
-      <h2>Quiz Summary for {{ userName }}</h2>
-      <p><strong>Questions Answered Correctly:</strong> {{ correctAnswers }}</p>
-      <p><strong>Total Questions:</strong> {{ totalQuestions }}</p>
-      <p><strong>Score:</strong> {{ score }}</p>
-      <router-link to="/">Back to Home</router-link>
+        <h2>Quiz Summary for {{ userName }}</h2>
+        <p><strong>Questions Answered Correctly:</strong> {{ correctAnswers }}</p>
+        <p><strong>Questions Answered Incorrectly:</strong> {{ totalQuestions-correctAnswers }}</p>
+        <p><strong>Total Questions:</strong> {{ totalQuestions }}</p>
+        <p><strong>Score:</strong> {{ score }}</p>
+
+        <router-link to="/">Back to Home</router-link>
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
+<script>
+export default {
     props: ['userName', 'correctAnswers', 'totalQuestions', 'score']
-  };
-  </script>
+};
+</script>
   
