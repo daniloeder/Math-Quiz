dcb287f008b2f7b0dee3bc841083c96866ae5c71
daniloeder <daniloeder1997@gmail.com>
2023-10-25 10:05:31 -0300

    Enhance QuizPage with the following features:
    - Add a 'Back to Home' button.
    - Clear the answer input when a new question is presented.
    - Display the current question number out of 10.
    - End the quiz after 10 questions.
    - Allow users to submit answers using the Enter key.
    - Improve timer management to automatically submit when time runs out and clear intervals/timeouts appropriately.

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>Welcome to Math Quiz!</h1>
+    <!-- Added a dropdown to select difficulty -->
+    <select v-model="difficulty">
+      <option value="easy">Easy</option>
+      <option value="medium">Medium</option>
+      <option value="hard">Hard</option>
+    </select>
     <button @click="startQuiz">Start Quiz</button>
     <router-link to="/scores">High Scores</router-link>
   </div>
@@ -8,9 +14,14 @@
 
 <script>
 export default {
+  data() {
+    return {
+      difficulty: 'easy'
+    };
+  },
   methods: {
     startQuiz() {
-      this.$router.push('/quiz');
+      this.$router.push({ path: '/quiz', query: { difficulty: this.difficulty } });
     }
   }
 };
--- src/components/QuizPage.vue
+++ src/components/QuizPage.vue
@@ -1,46 +1,84 @@
 <template>
   <div>
-    <h2>{{ question.text }}</h2>
-    <input v-model="answer" type="number" />
+    <h2>Question {{ questionNumber }} of 10: {{ question.text }}</h2>
+    <div v-if="timeLeft > 0">Time left: {{ timeLeft }}s</div>
+    <input v-model="answer" @keyup.enter="submitAnswer" type="number" />
     <button @click="submitAnswer">Submit</button>
     <div v-if="feedback">{{ feedback }}</div>
+    <router-link to="/">Back to Home</router-link>
   </div>
 </template>
 
 <script>
-// import { ref, computed } from "vue";
-
 export default {
   data() {
     return {
       question: {},
       answer: null,
-      feedback: ''
+      feedback: '',
+      questionNumber: 1, // Start with the first question
+      timeLeft: 10,
+      timerInterval: null, // to store the interval ID
+      timeoutId: null  // to store the timeout ID
     };
   },
   created() {
+    this.startTimer();
     this.generateQuestion();
   },
   methods: {
+    startTimer() {
+      if (this.timerInterval) {
+        clearInterval(this.timerInterval);
+      }
+
+      this.timeLeft = 10;
+
+      this.timerInterval = setInterval(() => {
+        if (this.timeLeft > 0) {
+          this.timeLeft--;
+        } else {
+          clearInterval(this.timerInterval);
+          this.submitAnswer();
+        }
+      }, 1000);
+    },
     generateQuestion() {
+      // Check if we've already completed 10 questions
+      if (this.questionNumber > 10) {
+        this.feedback = "Quiz completed!";
+        if (this.timerInterval) {
+          clearInterval(this.timerInterval);
+        }
+        return; // Don't generate another question
+      }
+
       const num1 = Math.floor(Math.random() * 10);
       const num2 = Math.floor(Math.random() * 10);
       this.question = {
         text: `What is ${num1} + ${num2}?`,
         answer: num1 + num2
       };
+      this.answer = ""; // Clear the previous answer
+      this.startTimer();
     },
     submitAnswer() {
+      if (this.timeoutId) {
+        clearTimeout(this.timeoutId); // Clear the previous timeout
+      }
+
       if (parseInt(this.answer) === this.question.answer) {
         this.feedback = "Correct!";
         this.$store.commit('incrementScore');
-        setTimeout(() => {
-          this.feedback = "";
-          this.generateQuestion();
-        }, 2000);
       } else {
-        this.feedback = "Incorrect. Try again.";
+        this.feedback = "Incorrect.";
       }
+
+      this.timeoutId = setTimeout(() => {
+        this.feedback = "";
+        this.questionNumber++;
+        this.generateQuestion();
+      }, 2000);
     }
   }
 };
